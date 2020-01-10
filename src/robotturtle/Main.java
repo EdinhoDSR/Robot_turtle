a4bbfa5a515f527b78a492c473444bc0586cc986
Edinhodsr <edinhodsr@gmail.com>
2020-01-10 13:32:26 +0100

    run() fait sans le break

--- src/robotturtle/Main.java
+++ src/robotturtle/Main.java
@@ -13,12 +13,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Joueur>listeDeJoueur = initialisation(2);
-        afficher();
-
-        for (Joueur joueur : listeDeJoueur) {
-            System.out.println(joueur.getNumero());
-        }
 
     }
 
@@ -106,9 +100,11 @@ public class Main {
     }
 
     public void run(){
-        while (nombreDeJoueurs >=1){
-            initialisation(nombreDeJoueurs);
-            tourDeJeu();
+        ArrayList<Joueur>listeDeJoueur = initialisation(2);
+        while (listeDeJoueur.size() >1){
+            for (Joueur joueur : listeDeJoueur) {
+                tourDeJeu(joueur);
+            }
         }
         System.out.println("Fin de la partie");
     }
@@ -160,142 +156,5 @@ public class Main {
     }
 
 
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//plateau[y][x]
-    public static robotturtle.Cases [][] plateau = new robotturtle.Cases[10][10];
-    private static Scanner scanner = new Scanner(System.in);
-    public static int nombreDeJoueurs;
-
-    public static void robotturtle.main(String[] args) {
-        System.out.println("Entrez le nombre de joueurs");
-        int nombreDeJoueurs = Integer.parseInt(scanner.nextLine());
-        initialisation(nombreDeJoueurs);
-        for(int i=0;i<10;i++){
-            for (int j=0;j<10;j++){
-                plateau[i][j]=
-
-
-            }
-
-        }
-        afficher();
-
-
-
-    }
-
-    private static void initialisation(int nombreJoueurs){
-        for (int x = 1; x < 9; x++) {//boucle qui fait le tableau jouable
-            for (int y = 1; y < 9; y++) {
-                plateau[y][x].setType('V');
-            }
-        }
-        for (int i = 0; i < 10; i++) {// boucle qui fait les bords
-            plateau[0][i].setType('O');
-            plateau[9][i].setType('O');
-            plateau[i][0].setType('O');
-            plateau[i][9].setType('O');
-
-        }
-        if (nombreJoueurs==2 || nombreJoueurs==3){// rajoute des bords s'il y a moins de 4 joueurs
-            for (int y = 0; y < 10; y++){plateau[y][8].setType('M');}
-        }
-
-        //Initialise les joueurs, les joyaux et leurs positions
-        if (nombreJoueurs==2){
-            plateau[8][4].joyau(8,4);
-
-            plateau[1][2].joueur('S',1,2,1);
-            plateau[1][6].joueur('S',1,6,2);
-
-
-        }
-        else if (nombreJoueurs==3){
-            plateau[8][1].joyau(8,4);
-            plateau[8][4].joyau(8,4);
-            plateau[8][7].joyau(8,7);
-
-            plateau[1][1].joueur('S',1,1,1);
-            plateau[1][4].joueur('S',1,4,2);
-            plateau[1][7].joueur('S',1,7,3);
-        }
-        else if (nombreJoueurs==4){
-            plateau[8][2].joyau(8,2);
-            plateau[8][7].joyau(8,7);
-
-            plateau[1][1].joueur('S',1,1,1);
-            plateau[1][3].joueur('S',1,3,2);
-            plateau[1][6].joueur('S',1,6,3);
-            plateau[1][8].joueur('S',1,8,4);
-        }
-
-    }
-
-
-
-
-
- */
-
-
-
-
-
-
-
-
-
-
-
-
 
